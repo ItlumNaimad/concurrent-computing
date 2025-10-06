23fe88f22d8d9e01844496327e2076e13f055d1c
ItlumNaimad <naimad2003@wp.pl>
2025-10-06 02:18:08 +0200

    Add interface of view the list

--- wikicount/src/main/kotlin/Main.kt
+++ wikicount/src/main/kotlin/Main.kt
@@ -1,7 +1,24 @@
 package org.example
 
+/**
+ * URLEncoder.encode zamienia spacje na '+' domyślnie, ale Wikipedia używa '_'.
+ * Po zakodowaniu, możemy ręcznie zamienić '+' na '_', a jeśli trzeba też inne znaki.
+ * Lepszym podejściem jest prosta zamiana spacji na podkreślnik, bo Wikipedia jest dość liberalna w tej kwestii.
+ * @param: term :String
+ * @return: url: String
+*/
+fun buildWikipediaUrl(term: String): String {
+    val formattedTerm = term.replace(' ', '_')
+    return "https://pl.wikipedia.org/wiki/$formattedTerm"
+}
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+/**
+ * Główna funkcja programu. Interfejs użytkownika
+ * @param: null
+ * @return: null
+ * @author: Damian Skonieczny
+ */
 fun main() {
     // val name = "Kotlin"
     //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
@@ -14,6 +31,9 @@ fun main() {
 
     var depth: Int? = null
     while(depth == null){
+        /* Odczytaj input z klawiatury, przypisz do zmiany @depth.
+        * Konwertuj typ zmiany do Int lub nadaj Null
+        */
         depth = readln().toIntOrNull()
         if(depth == null){
             println("To nie jest poprawna liczba. Spróbuj ponownie.")
@@ -21,5 +41,5 @@ fun main() {
     }
 
     println("Rozpoczynam szukanie dla '$termin' z głębokością $depth...")
-    scraper(termin)
+    titleScraper(buildWikipediaUrl(termin.lowercase()))
 }
--- wikicount/src/main/kotlin/Scraper.kt
+++ wikicount/src/main/kotlin/Scraper.kt
@@ -3,8 +3,20 @@ package org.example
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
 
-fun scraper(termin: String) {
+/**
+ * Wyodrębnia i drukuje zawartość znacznika <title> ze strony Wikipedii
+ * odpowiadającą podanemu wyszukiwanemu hasłu.
+ * @see <a href="https://jsoup.org/">
+ * @param termin  wyszukiwania użyty do skonstruowania adresu URL strony Wikipedii
+ */
+fun titleScraper(termin: String) {
+    /**
+     * Funkcja JSoup typu Dokument odpowiedzialna za pobranie
+     * kodu HTML strony
+     * @see <a href="https://jsoup.org/cookbook/input/load-document-from-url">
+    */
     val doc: Document = Jsoup.connect("https://pl.wikipedia.org/$termin").get()
+    // Pobieranie zawartości znacznika <title>
     val title: String? = doc.title()
     println(title)
 }
