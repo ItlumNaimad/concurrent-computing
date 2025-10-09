b3bff9f6d5cbbbb4bb76afb11dcc435208d56446
ItlumNaimad <naimad2003@wp.pl>
2025-10-09 23:53:45 +0200

    Wyszukiwanie linków w głąb + BFS

--- wikicount/src/main/kotlin/Main.kt
+++ wikicount/src/main/kotlin/Main.kt
@@ -1,45 +1,31 @@
 package org.example
 
-/**
- * URLEncoder.encode zamienia spacje na '+' domyślnie, ale Wikipedia używa '_'.
- * Po zakodowaniu, możemy ręcznie zamienić '+' na '_', a jeśli trzeba też inne znaki.
- * Lepszym podejściem jest prosta zamiana spacji na podkreślnik, bo Wikipedia jest dość liberalna w tej kwestii.
- * @param: term :String
- * @return: url: String
-*/
-fun buildWikipediaUrl(term: String): String {
-    val formattedTerm = term.replace(' ', '_')
-    return "https://pl.wikipedia.org/wiki/$formattedTerm"
-}
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+
 /**
  * Główna funkcja programu. Interfejs użytkownika
- * @param: null
- * @return: null
  * @author: Damian Skonieczny
  */
 fun main() {
-    // val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Witaj użytkowniku. Podaj hasło, do przeszukiwań")
-
+    println("Witaj użytkowniku. Podaj hasło do przeszukiwania (np. Fortepian)")
     val termin = readln()
 
     println("A teraz proszę o podanie głębokości przeszukiwania (np. 2)")
-
     var depth: Int? = null
-    while(depth == null){
-        /* Odczytaj input z klawiatury, przypisz do zmiany @depth.
-        * Konwertuj typ zmiany do Int lub nadaj Null
-        */
+    while (depth == null || depth <= 0) {
         depth = readln().toIntOrNull()
-        if(depth == null){
-            println("To nie jest poprawna liczba. Spróbuj ponownie.")
+        if (depth == null || depth <= 0) {
+            println("To nie jest poprawna liczba dodatnia. Spróbuj ponownie.")
         }
     }
 
-    println("Rozpoczynam szukanie dla '$termin' z głębokością $depth...")
-    titleScraper(buildWikipediaUrl(termin.lowercase()))
+    println("\nRozpoczynam szukanie dla '$termin' z głębokością $depth...")
+
+    // Wywołaj główną funkcję scrapującą
+    val results = findWikipediaLinks(termin, depth)
+
+    // Wydrukuj wyniki za pomocą funkcji z Interface.kt
+    printResults(results)
 }
--- wikicount/src/main/kotlin/Scraper.kt
+++ wikicount/src/main/kotlin/Scraper.kt
@@ -1,22 +1,117 @@
 package org.example
 
+import org.jsoup.HttpStatusException
 import org.jsoup.Jsoup
-import org.jsoup.nodes.Document
+import java.net.URLDecoder
+import java.util.ArrayDeque
 
 /**
- * Wyodrębnia i drukuje zawartość znacznika <title> ze strony Wikipedii
- * odpowiadającą podanemu wyszukiwanemu hasłu.
- * @see <a href="https://jsoup.org/">
- * @param termin  wyszukiwania użyty do skonstruowania adresu URL strony Wikipedii
+ * URLEncoder.encode zamienia spacje na '+' domyślnie, ale Wikipedia używa '_'.
+ * Po zakodowaniu, możemy ręcznie zamienić '+' na '_', a jeśli trzeba też inne znaki.
+ * Lepszym podejściem jest prosta zamiana spacji na podkreślnik, bo Wikipedia jest dość liberalna w tej kwestii.
+ * @param: term :String
+ * @return: url: String
  */
-fun titleScraper(termin: String) {
-    /**
-     * Funkcja JSoup typu Dokument odpowiedzialna za pobranie
-     * kodu HTML strony
-     * @see <a href="https://jsoup.org/cookbook/input/load-document-from-url">
-    */
-    val doc: Document = Jsoup.connect("https://pl.wikipedia.org/$termin").get()
-    // Pobieranie zawartości znacznika <title>
-    val title: String? = doc.title()
-    println(title)
+
+private fun buildWikipediaUrl(articleTitle: String): String {
+    // Spacje zamieniamy na podkreślniki, to standard w URL-ach Wiki.
+    val formattedTerm = articleTitle.replace(' ', '_')
+    return "https://pl.wikipedia.org/wiki/$formattedTerm"
+}
+
+/**
+ * Funkcja pomocnicza, która dla danego tytułu artykułu pobiera stronę
+ * i wyciąga z niej wszystkie unikalne linki do innych artykułów.
+ *
+ * @param articleTitle Tytuł artykułu do przeskanowania.
+ * @return Zbiór (Set) tytułów artykułów znalezionych na stronie.
+ */
+private fun extractArticleLinks(articleTitle: String): Set<String> {
+    val foundLinks = mutableSetOf<String>()
+    try {
+        val url = buildWikipediaUrl(articleTitle)
+        val doc = Jsoup.connect(url).get()
+
+        // Selektor CSS:
+        // #mw-content-text -> wybierz główny kontener z treścią artykułu
+        // a[href] -> wewnątrz niego znajdź wszystkie znaczniki <a>, które mają atrybut href
+        val links = doc.select("#mw-content-text a[href]")
+
+        for (link in links) {
+            val href = link.attr("href")
+
+            // Filtrujemy linki, które nas interesują:
+            // 1. Muszą zaczynać się od "/wiki/" - to standard dla linków do artykułów.
+            // 2. Nie mogą zawierać ":" - to eliminuje strony specjalne (np. "Pomoc:", "Plik:", "Wikipedia:").
+            // 3. Nie mogą być linkiem do strony głównej.
+            if (href.startsWith("/wiki/") && !href.contains(":") && href != "/wiki/Strona_główna") {
+                // Wyciągamy sam tytuł z linku (usuwamy "/wiki/")
+                val title = href.substringAfter("/wiki/")
+                // Dekodujemy tytuł, aby zamienić np. %C4%85 na "ą"
+                val decodedTitle = URLDecoder.decode(title, "UTF-8")
+                foundLinks.add(decodedTitle)
+            }
+        }
+    } catch (e: HttpStatusException) {
+        // Ignorujemy błędy typu 404 (strona nie znaleziona)
+        println("! Nie można odnaleźć strony dla: $articleTitle (błąd ${e.statusCode})")
+    } catch (e: Exception) {
+        // Inne błędy (np. brak połączenia)
+        println("! Wystąpił błąd podczas przetwarzania $articleTitle: ${e.message}")
+    }
+    return foundLinks
+}
+
+/**
+ * Główna funkcja scrapująca. Realizuje algorytm BFS do znalezienia wszystkich
+ * artykułów w zasięgu `maxDepth` od artykułu `startTerm`.
+ *
+ * @param startTerm Tytuł artykułu startowego.
+ * @param maxDepth Maksymalna głębokość przeszukiwania.
+ * @return Mapa, gdzie kluczem jest poziom głębokości, a wartością zbiór tytułów artykułów.
+ */
+fun findWikipediaLinks(startTerm: String, maxDepth: Int): Map<Int, Set<String>> {
+    // Mapa do przechowywania wyników dla każdego poziomu
+    val results = mutableMapOf<Int, MutableSet<String>>()
+    // Kolejka do zarządzania artykułami do odwiedzenia (rdzeń BFS)
+    // Przechowujemy parę: (Tytuł Artykułu, Obecna Głębokość)
+    val queue = ArrayDeque<Pair<String, Int>>()
+    // Zbiór do śledzenia już odwiedzonych artykułów, aby unikać pętli i powtórzeń
+    val visited = mutableSetOf<String>()
+
+    // Inicjalizacja algorytmu
+    queue.add(startTerm to 0)
+    visited.add(startTerm)
+
+    println("Rozpoczynam przeszukiwanie...")
+
+    // Pętla działa, dopóki w kolejce są jeszcze jakieś artykuły do przetworzenia
+    while (queue.isNotEmpty()) {
+        val (currentTitle, currentDepth) = queue.removeFirst()
+
+        println("Przetwarzam: $currentTitle (głębokość: $currentDepth)")
+
+        // Jeśli osiągnęliśmy maksymalną głębokość, nie szukamy już dalej z tego miejsca
+        if (currentDepth >= maxDepth) {
+            continue
+        }
+
+        // Znajdź wszystkie linki na bieżącej stronie
+        val newLinks = extractArticleLinks(currentTitle)
+
+        for (linkTitle in newLinks) {
+            // Jeśli jeszcze nie odwiedzaliśmy tego linku...
+            if (linkTitle !in visited) {
+                // ...dodaj go do już odwiedzonych
+                visited.add(linkTitle)
+                // Dodaj go do kolejki do przetworzenia w przyszłości
+                val nextDepth = currentDepth + 1
+                queue.add(linkTitle to nextDepth)
+                // Zapisz wynik na odpowiednim poziomie głębokości
+                results.getOrPut(nextDepth) { mutableSetOf() }.add(linkTitle)
+            }
+        }
+    }
+    println("Zakończono przeszukiwanie.")
+    return results
 }
